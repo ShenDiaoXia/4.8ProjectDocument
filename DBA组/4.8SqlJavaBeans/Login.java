14b53911d5939e4292306ecdd1f6e073f8aa7710
yangzebo <1325813810@qq.com>
2019-04-17 19:29:06 +0800

    4.8SqlJavaBeans

---
+++ DBA组/4.8SqlJavaBeans/Login.java
@@ -0,0 +1,27 @@
+package Beans;
+
+public class Login {
+     private int acct_id;
+     private String account;
+     private String password;
+	public int getAcct_id() {
+		return acct_id;
+	}
+	public void setAcct_id(int acct_id) {
+		this.acct_id = acct_id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	}
+

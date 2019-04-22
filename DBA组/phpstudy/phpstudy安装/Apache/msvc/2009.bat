4dad641bb1151e4ce44c0a386edf05ea9d497910
yangzebo <1325813810@qq.com>
2019-04-22 21:22:29 +0800

    上传Sql语句和表格

--- DBA组/phpstudy/phpstudy安装/Apache/msvc/2009.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-cd apache/msvc
-vc_red.msi /qf
-EXIT
--- DBA组/phpstudy/phpstudy安装/tmp/清空临时文件.bat
+++
@@ -1,5 +0,0 @@
-del /f /q /s eAccelerator\*.*
-del /f /q /s tmp\*.*
-del /f /q /s XCache\*.*
-del /f /q /s xdebug\*.*
-del /f /q /s jianshilog\*.*
